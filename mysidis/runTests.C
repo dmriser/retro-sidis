3d403cf662362a5ba4c9655135df2eaeb16c21bf
David Riser <dmriser@jlab.org>
2018-08-13 13:39:31 -0400

    A lot of changes to mysidis

---
+++ mysidis/runTests.C
@@ -0,0 +1,19 @@
+#include "mysidis.C"
+
+void runTests(){
+
+  gROOT->LoadMacro("mysidis.C"); 
+
+  // We're go for run. 
+  // Incomprehensible. 
+
+  /*
+  mysidis(0, 1, 2, 1, 1, 1, 0, 0, 0,
+	  0, 0, 0, 0, 0, 0, 0, 0, 0, 
+	  0, 0, 0, 0);
+  */
+  mysidis(0, 1, 2, 0, 1, 1, 0, 0, 0,
+	  0, 0, 0, 0, 0, 0, 0, 0, 0, 
+	  0, 0, 0, 0);
+
+}
