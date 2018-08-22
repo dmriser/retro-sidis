b9f75fb1f7f1ee06c1ad50d21f76557dc6496809
David Riser <dmriser@jlab.org>
2018-08-22 13:59:57 -0400

    Adding sdfasdf

--- farm/create_conf.py
+++ farm/create_conf.py
@@ -1,14 +1,17 @@
 import json 
 
 config = {}
-
 config['base_path'] = '.'
 config['project_name'] = 'TestProject'
 config['variation'] = 'nominal'
 config['job_id'] = 9
 config['data_type'] = 'data'
+config['change_strictness'] = 0
 config['strict_to_change'] = 0
 config['strictness'] = 0
+config['correct_electron_p'] = 1
+config['correct_pion_p'] = 1
+config['acceptance_iteration'] = 0
 config['files'] = ['/volatile/clas12/dmriser/data/all/root/37748.root',
                    '/volatile/clas12/dmriser/data/all/root/37750.root',
                    '/volatile/clas12/dmriser/data/all/root/37753.root',
--- farm/run.py
+++ farm/run.py
@@ -4,40 +4,32 @@ import glob
 import sys, os
 import subprocess
 import json 
-import tempfile 
-
-def setup_analysis():
-    os.system('cp -r /u/home/dmriser/clas/retro-sidis/mysidis/* .')
-
-def run():
-    os.system('root -b -q run.cpp')
-
-def rename():
-    root_files = glob.glob('*.root')
-    
-    for index, root_file in enumerate(root_files):
-        base_name = root_file.split('.root')[0]
-
-        os.system('mv {} {}.part{}.root'.format(root_file, base_name, index))
-
-    return glob.glob('*.root')
 
 if __name__ == '__main__':
 
+    # Setup code. 
+    os.system('cp -r /u/home/dmriser/clas/retro-sidis/mysidis/* .')
+
+    # This should be provided for each job. 
     with open('config.json') as input_json_file:
         config = json.load(input_json_file)
 
-        # Create a tempfile that contains the files
-        # for analysis. 
-        run_list = tempfile.NamedTemporaryFile() 
-        for f in config['files']:
-            run_list.write(f)
-
-        n_files = len(config['files'])
+        with open('temp_runfile.dat', 'w') as run_list:
+            for f in config['files']:
+                run_list.write('{}\n'.format(f))
+                
+        number_of_files = len(config['files'])
         is_data = int(config['data_type'] == 'data')
-        root_cmd = 'root -b -q \'mysidis.C("{}",{},{},0,1,1,{},{},{})\''.format(
-            run_list.name, n_files, is_data, config['change_strict'], 
-            config['strict_to_change'], config['strictness'])
-        print(root_cmd)
+        root_cmd = 'root -b -q \'mysidis.C("{}",{},{},{},{},{},{},{},{})\''.format(
+            run_list.name, 
+            number_of_files, 
+            is_data, 
+            config['acceptance_iteration'],
+            config['correct_electron_p'],
+            config['correct_pion_p'],
+            config['change_strictness'], 
+            config['strict_to_change'], 
+            config['strictness'])
 
-        run_list.close() 
+        os.system(root_cmd)
+        os.system('rm temp_runfile.dat')
