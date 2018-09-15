c8c4d8d07df48e30fd455e6be966b5c07286194d
David Riser <dmriser@jlab.org>
2018-09-15 08:02:14 -0400

    IDKbro

---
+++ mysidis/download_required_files.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python 
+
+import argparse 
+import os 
+
+def setup_storage_dir(storage_directory):
+    ''' Setup the main directory to store large files. '''
+    os.mkdir(storage_directory)
+
+def setup_sub_dirs(storage_directory):
+    os.mkdir('{}/data'.format(storage_directory))
+    os.mkdir('{}/mc'.format(storage_directory))
+
+if __name__ == '__main__':
+
+    ap = argparse.ArgumentParser(description='This utility automates the process of getting the files required to perform analysis on the E1-F dataset using the macros of this repository.')
+    ap.add_argument('-s', '--storage_directory', required=True, help='This should be a large storage area for data files, as well as analysis files.')
+    args = ap.parse_args()
+
+    # Check for file structure. 
+    if os.path.exists(args.storage_directory):
+
+        if os.path.exists('{}/data'.format(args.storage_directory)) and os.path.exists('{}/mc'.format(args.storage_directory)):
+            pass
+        else:
+            setup_sub_dirs(args.storage_directory)
+
+    else:
+        setup_storage_dir(args.storage_directory)
+        setup_sub_dirs(args.storage_directory)
+
+        print('jget /mss/home/dmriser/data/all/root/* {}/data/'.format(args.storage_directory))
+        print('jget /mss/home/dmriser/data/all/root/* {}/mc/'.format(args.storage_directory))
+
+    os.system('jget /mss/home/dmriser/data/all/root/* {}/data/'.format(args.storage_directory))
+    os.system('jget /mss/home/dmriser/data/all/root/* {}/mc/'.format(args.storage_directory))
+
+
+    # Unload required files. 
+    os.system('tar -xzvf requiredFiles.tar')
+    os.system('rm requiredFiles/lists/*')
+    os.system('ls {}/data/* > requiredFiles/lists/data.dat'.format(args.storage_directory))
+    os.system('ls {}/mc/* > requiredFiles/lists/mc.dat'.format(args.storage_directory))
